584fc53a0e808641450ce12f202ab8449dbd9edb
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-05-04 09:24:05 -0300

    add react materialize and css

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import {Button, Icon} from 'react-materialize';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        React simple starter
+        <Button node='a' waves='light'><Icon right>file_cloud</Icon>button</Button>
+        <Button floating large className='red' waves='light' icon='add' />
+      </div>
     );
   }
 }
